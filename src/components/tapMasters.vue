323e3dba0b36f7dbf83a58394954cd86bec6a950
Serge018 <sivenskij@gmail.com>
2019-12-03 22:19:03 +0300

    add scripts for 'tapMasters' component

--- src/components/tapMasters.vue
+++ src/components/tapMasters.vue
@@ -1,29 +1,44 @@
 <template>
-  <div id="tap-masters">
+  <div id="tap-masters" @click="runIntreval">
   	<div class="icons">
   		<div 
   			v-for="name in names" 
   			:class="'icon icon-'+name"
         :data-persone="name"
         @click="choosePerson"
-  		></div>
+  		>
+        <svg width="118" height="118" viewBox="0 0 118 118" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <circle class="bp bp-1" cx="59" cy="59" r="58" stroke="#ffbb33" fill="transparent" stroke-width="2"/>
+          <circle class="bp bp-2" cx="50" cy="50" r="49" stroke="#ffbb33" fill="transparent" stroke-width="2"/>
+          <circle class="bp bp-3" cx="44" cy="44" r="43" stroke="#ffbb33" fill="transparent" stroke-width="2"/>
+          <circle class="bp bp-4" cx="41" cy="41" r="40" stroke="#ffbb33" fill="transparent" stroke-width="2"/>
+        </svg>
+      </div>
   	</div>
   	<div class="container-content">
   		<div 
-  			v-for="name in names" 
         class="wrap-content"
-  			:class="['wrap-content-'+name, { active: (name === 'Misha') ? isActive : !isActive}]"
+  			v-for="name in names" 
+  			:class="'wrap-content-'+name"
   		>
-      <div class="cont-name" :class="'cont-name-'+name"></div>
-      <div class="discript discript-master">{{ discript[name] }}</div>
+      <div 
+        class="cont-name" 
+        :class="['cont-name-'+name, { active: (name === 'Misha') ? isActive : !isActive}]"
+      ></div>
+      <div class="discript-master">
+        <span 
+          class="discript discript-text"
+          :class="{ active: (name === 'Misha') ? isActive : !isActive}"
+        >{{ discript[name] }}</span>
+      </div>
   		</div>
   	</div>
-  	
   </div>
 </template>
 
 <script>
 import drawTitle from '../utils/drawTitle.js';
+import helpers from '../utils/helpers.js';
 	
 export default {
 	name: 'TapMasters',
@@ -39,19 +54,48 @@ export default {
         Misha: this.$props.text.discriptMisha,
         Vova: this.$props.text.discriptVova
       },
-      currentName: "Misha",
+      nodeIconMisha: null,
+      nodeIconVova: null,
+      setNodeIcons: false,
+      currentName: "",
       flagAutoPlay: true,
-      isActive: true
+      isActive: true,
+      interval: null
 		}
 	},
   computed: {},
   methods: {
     choosePerson: function(e) {
-      const persone = e.target.dataset.persone;
-      if (persone !== this.currentName) {
-        this.currentName = persone;
+      const 
+        currentTarget = e.target.closest(".icon"),
+        selectedPersone = currentTarget.dataset.persone;
+
+      if (this.flagAutoPlay) {
+        this.flagAutoPlay = false;
+        clearInterval(this.interval);
+        this.currentName = selectedPersone;
+        this.isActive = (selectedPersone === 'Misha') ? true : false;
+        helpers.removeClass(this.setNodeIcons, 'animateIcon');
+        currentTarget.parentElement.classList.add('clicked');
+      } else {
+        helpers.removeClass(this.setNodeIcons, 'selected');
+      }
+
+      currentTarget.classList.add('selected');
+      if (selectedPersone !== this.currentName) {
+        this.currentName = selectedPersone;
         this.isActive = !this.isActive;
       }
+    },
+    runIntreval: function() {
+      /*this.flagAutoPlay = true;
+      this.nodeIconMisha.classList.add('animateIcon');
+      this.interval = setInterval(() => {
+        if (this.flagAutoPlay) {
+          helpers.toggleClass(this.setNodeIcons, 'animateIcon');
+          this.isActive = !this.isActive;
+        }
+      }, 7100);*/
     }
   },
   mounted() {
@@ -64,11 +108,26 @@ export default {
         type: "sub-title",
         text: titleText, 
         width: 300, 
-        height: 100,
+        height: 110,
         wordWrap: true,
         fillGradientStops: [0.2, 1]
       });
     });
+
+    this.nodeIconMisha = this.$el.querySelector(`.icon-Misha`);
+    this.nodeIconVova = this.$el.querySelector(`.icon-Vova`);
+    this.setNodeIcons = [this.nodeIconMisha, this.nodeIconVova];
+
+
+    // the temporare plug
+    this.flagAutoPlay = true;
+    this.nodeIconMisha.classList.add('animateIcon');
+    this.interval = setInterval(() => {
+      if (this.flagAutoPlay) {
+        helpers.toggleClass(this.setNodeIcons, 'animateIcon');
+        this.isActive = !this.isActive;
+      }
+    }, 7250);
   }
 }
 </script>
@@ -88,6 +147,10 @@ export default {
         display: inline-block;
         cursor: pointer;
         background-size: contain;
+        position: relative;
+        overflow: hidden;
+        transform-origin: 50% 50%;
+        transform: rotate(-90deg);
 
         &-Misha {
           background-image: url('../assets/img/Michail_icon/oval@2x.png');
@@ -97,26 +160,75 @@ export default {
           background-image: url('../assets/img/Vovan_icon/oval@2x.png');
           margin-left: 1.235rem;
         }
+
+        &.animateIcon {
+          svg {
+            transition: opacity 250ms linear 250ms;
+            opacity: 1;
+
+            .bp {
+              transition: stroke-dashoffset 7000ms linear 250ms;
+              stroke-dashoffset: 0px;
+            }
+          }
+        }
+
+        &.selected {
+          svg {
+            transition: opacity 250ms linear 250ms;
+            opacity: 1;
+            
+            .bp {
+              transition: none;
+              stroke-dashoffset: 0px;
+            }
+          }
+        }
+
+        svg {
+          position: absolute;
+          left: 0px;
+          top: 0px;
+          transition: opacity 250ms linear;
+          opacity: 0;
+
+          .bp {
+
+            &-1 {
+              stroke-dashoffset: 364px;
+              stroke-dasharray: 364px;
+            }
+
+            &-2 {
+              stroke-dashoffset: 307px;
+              stroke-dasharray: 307px;
+            }
+
+            &-3 {
+              stroke-dashoffset: 270px;
+              stroke-dasharray: 270px;
+            }
+
+            &-4 {
+              stroke-dashoffset: 251px;
+              stroke-dasharray: 251px;
+            }
+          }
+        }
       }
 		}
 
     .container-content {
       position: relative;
       height: 14.117rem;
-      
+
+
       .wrap-content {
         position: absolute;
         top: 0px;
         left: 0px;
         width: 100%;
         height: 100%;
-        transition: opacity 250ms linear;
-        opacity: 0;
-
-        &.active {
-          opacity: 1;
-          transition: opacity 250ms linear 200ms;
-        }
       }
 
       .discript-master {
@@ -128,7 +240,17 @@ export default {
         padding-left: 3.529rem;
         border-left: 3px solid var(--gold-color);
       }
-    }
+      
+      .cont-name,
+      .discript-text {
+        transition: opacity 250ms linear;
+        opacity: 0;
 
+        &.active {
+          opacity: 1;
+          transition: opacity 250ms linear 200ms;
+        }
+      }
+    }
 	}
 </style>
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,28 @@
 export default {
 
-
 	getPropStyle: function(el, nameProp) {
     return parseInt(getComputedStyle(el)[nameProp]);
+  },
+
+
+  removeClass: function(nodes, className) {
+  	if (Array.isArray(nodes)) {
+  		nodes.forEach(node => {
+        if (node.classList.contains(className)) {
+          node.classList.remove(className);
+        }
+      });
+  	} else {
+  		nodes.classList.remove(className);
+  	}
+  },
+  toggleClass: function(nodes, className) {
+  	if (Array.isArray(nodes)) {
+  		nodes.forEach(node => node.classList.toggle(className))
+  	} else {
+  		nodes.classList.toggle(className);
+  	}
   }
-  
-  
+
+
 }
